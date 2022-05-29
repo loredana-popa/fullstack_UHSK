390a13e5e36907af25e68a4fd5f46eff39c8e995
loredana-popa <loredana.iacob87@gmail.com>
2022-05-29 14:42:08 +0300

    finished task  1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,9 +4,15 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  <p>{props.cPart} {props.nrExercises}</p>
+}
+
 const Content = (props) => {
   return (
-    <p>{props.part} {props.exercises}</p>
+    <div>
+      <Part cPart={props.part} nrExercises={props.exercises}/>
+    </div>
   )
 }
 
@@ -28,8 +34,8 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      < Content part={part1} exercises={exercises1} />
-      < Content part={part2} exercises={exercises2} />
+      < Content part={part1} exercises={exercises1}/>
+      < Content part={part2} exercises={exercises2}/>
       < Content part={part3} exercises={exercises3} />
       <Total totalEx={exercises1 + exercises2 + exercises3} />
     </div>
