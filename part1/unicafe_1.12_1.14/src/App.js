998dfe5e4f6c48638c1053bb7f839797ff0dbfc9
loredana-popa <loredana.iacob87@gmail.com>
2022-07-19 10:21:04 +0300

    task 1.13

--- part1/unicafe_1.12_1.14/src/App.js
+++ part1/unicafe_1.12_1.14/src/App.js
@@ -12,21 +12,45 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
-
-  const getNewAnecdote = () => {
-    const max = anecdotes.length - 1;
-    const min = 0;
-    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-   
-    return setSelected(randomNumber)
-    // console.log(randomNumber)
-    
+ 
+  // Anecdotes -> Votes(list of vote)
+  // vote is key-value pair where:
+  //         key is index in anecdote , value is Number
+  // 
+  const initializeVotes = (anecdotes) => {
+    return Array(anecdotes.length).fill(0)  
   }
 
+  const [votes, setVotes] = useState(initializeVotes(anecdotes))
+
+  function generateNumber() {
+    const max = anecdotes.length - 1;
+    const min = 0;
+    const  randomNumber= Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomNumber
+  }
+
+  function getNewAnecdote() {
+   setSelected(generateNumber())
+  }
+
+
+  // update votes when the vote button is clicked (increase votes by one)
+  const updateVotes = (selected, votes) => {
+    let copyVotes = [...votes]
+    copyVotes[selected] += 1
+    setVotes(copyVotes)
+  } 
+
+  
+  console.log(selected)
+  console.log(votes)
 
   return (
     <div>
       {anecdotes[selected]}
+      <div> has {votes[selected]} votes</div>
+      <button onClick={() => updateVotes(selected, votes)}>vote</button>
       <button onClick={() => getNewAnecdote()}>next anecdote</button>
     </div>
   )
