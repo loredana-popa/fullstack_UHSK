a8d71e790b7ace3a5c938f3b6dda40049bbf8740
loredana-popa <loredana.iacob87@gmail.com>
2022-07-26 22:49:40 +0300

    2.9*: The Phonebook Step4

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -10,12 +10,17 @@ const Person = ({ person }) =>{
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] =useState('')
+  const [searchText, setSearchText] =useState('')
 
   const addPerson = (event) =>{
     event.preventDefault()
@@ -40,19 +45,34 @@ const App = () => {
     
 
   const handlePersonChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+
+  const handleTextChange = (event) => {
+    setSearchText(event.target.value)
+  }
   
+  function searchPerson(person) {
+   if (person.name.toLowerCase().match(searchText.toLowerCase()) !==null) {
+     return person
+   } 
+ }
+  let resultArray =[...persons];
+  resultArray = persons.filter(searchPerson)
   
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+       <input value={searchText} onChange = {handleTextChange}/>
+      </div>
+      <h2>add new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -72,8 +92,10 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
   
-      {persons.map(person =>
-        <Person key={person.name} person={person}/>
+      {resultArray.map(person =>
+        <Person 
+          key={person.name}
+          person={person} />
       )}
     
     
