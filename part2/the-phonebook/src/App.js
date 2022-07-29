42f9b8c8665f62a9b46b97231b93e0f0732f3bee
loredana-popa <loredana.iacob87@gmail.com>
2022-07-29 17:13:33 +0300

    2.11: The Phonebook Step6

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -1,5 +1,6 @@
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ value, onChange}) => {
   return (
@@ -32,18 +33,22 @@ const Person = ({ person }) =>{
   
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
-
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] =useState('')
   const [searchText, setSearchText] =useState('')
+
+useEffect(() => {
+  console.log('effect')
+  axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promis fulfilled')
+      setPersons(response.data)
+    })
+}, [])
+console.log('render', persons.length, 'persons')
+
 
   const addPerson = (event) =>{
     event.preventDefault()
