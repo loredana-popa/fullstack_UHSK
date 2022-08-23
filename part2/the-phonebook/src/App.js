d9a64d833c503a6eb95972677c8209830f344feb
loredana-popa <loredana.iacob87@gmail.com>
2022-08-23 15:11:11 +0300

    2.15: Phonebook step7: save the numbers to backend server

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -1,6 +1,7 @@
 
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 const Filter = ({ value, onChange}) => {
   return (
@@ -38,24 +39,35 @@ const App = () => {
   const [newNumber, setNewNumber] =useState('')
   const [searchText, setSearchText] =useState('')
 
-useEffect(() => {
-  console.log('effect')
-  axios
-    .get('http://localhost:3001/persons')
-    .then(response => {
-      console.log('promis fulfilled')
-      setPersons(response.data)
-    })
-}, [])
-console.log('render', persons.length, 'persons')
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promis fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
 
-  const addPerson = (event) =>{
+  const addPerson = (event) => {
     event.preventDefault()
-    const personObject ={
+
+    const personObject = {
       name: newName,
       number: newNumber
     }
+
+    axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+        
+      })
+
     const isPerson = persons.find(element => {
       if (element.name === newName){
         return true;
@@ -84,6 +96,7 @@ console.log('render', persons.length, 'persons')
     setSearchText(event.target.value)
   }
   
+
   function searchPerson(person) {
    if (person.name.toLowerCase().match(searchText.toLowerCase()) !==null) {
      return person
@@ -115,7 +128,7 @@ console.log('render', persons.length, 'persons')
       />
 
       <div>
-        <button type="submit">add</button>
+        <button type="submit" onClick={addPerson}>add</button>
       </div>
 
       <h2>Numbers</h2>
