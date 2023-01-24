c0c757747de9e5ce4369c58281d03008fd7a5409
loredana.iacob87@gmail.com <loredana.iacob87@gmail.com>
2023-01-24 20:05:59 +0200

    6.1: unicafe revisited, step1 & 6.2: unicafe revisited, step2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -6,21 +6,26 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    const elValue = e.target.value
+
     store.dispatch({
-      type: 'GOOD'
+      type: `${elValue}`
     })
   }
 
+
   return (
     <div>
-      <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={handleClick} value='GOOD'>good</button>
+      <button onClick={handleClick} value='OK'>ok</button>
+      <button onClick={handleClick} value='BAD'>bad</button>
+      <button onClick={handleClick} value='ZERO'>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -32,4 +32,55 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('reset the counter', () => {
+    const state = {
+      good: 5,
+      ok: 3,
+      bad: 7
+    }
+
+    const action = {
+      type: 'ZERO'
+    }
+    
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
 })
