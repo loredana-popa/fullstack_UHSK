bceb68509331a44442c08f84da112a7528be3625
user userFamilyName <user.userfamilyname@gmail.com>
2023-02-17 15:40:53 +0200

    7.1: routed anecdotes, step1

---
+++ part6/routed-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,9 @@
+const AnecdoteList = ({ anecdotes }) => (
+    <div>
+      <h2>Anecdotes</h2>
+      <ul>
+        {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      </ul>
+    </div>
+  )
+export default AnecdoteList  
