54711320ad6b9b241d460e8f0173a4b0cb54b84e
user userFamilyName <user.userfamilyname@gmail.com>
2023-04-05 13:28:03 +0300

    Tasks: 8.1: The number of books and authors && 8.2: All books

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -28,17 +28,9 @@ let authors = [
 ]
 
 /*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
  * English:
  * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
  * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conección con el libro
  */
 
 let books = [
@@ -93,19 +85,34 @@ let books = [
 	},
 ]
 
-/*
-  you can remove the placeholder query once your first own has been implemented 
-*/
-
 const typeDefs = `
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+  }
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
   type Query {
-    dummy: Int
+    bookCount:Int!
+    authorCount: Int!
+    allBooks: [Book!]!
+    allAuthors : [Author!]!
   }
 `
 
 const resolvers = {
 	Query: {
-		dummy: () => 0,
+		bookCount: () => books.length,
+		authorCount: () => authors.length,
+		allBooks: () => books,
+		allAuthors: () => authors,
 	},
 }
 
