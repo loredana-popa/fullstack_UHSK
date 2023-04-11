6d792f3fd869a762e7e08cc546b951dfb98c8110
user userFamilyName <user.userfamilyname@gmail.com>
2023-04-11 15:20:10 +0300

    Connected app to MongoDB implemented functionality to save book and author in DB

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,30 +1,51 @@
-const Authors = (props) => {
-  if (!props.show) {
-    return null
-  }
-  const authors = []
+import { gql, useQuery } from '@apollo/client'
 
-  return (
-    <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>born</th>
-            <th>books</th>
-          </tr>
-          {authors.map((a) => (
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+const ALL_AUTHORS = gql`
+	query {
+		allAuthors {
+			name
+			born
+			bookCount
+		}
+	}
+`
+const Authors = props => {
+	const { loading, error, data } = useQuery(ALL_AUTHORS)
+	const authors = !data ? [] : data.allAuthors
+
+	if (loading) {
+		return <div>'Loading...'</div>
+	}
+
+	if (error) {
+		return <div>`Error! ${error.message}`</div>
+	}
+
+	if (!props.show) {
+		return null
+	}
+
+	return (
+		<div>
+			<h2>authors</h2>
+			<table>
+				<tbody>
+					<tr>
+						<th></th>
+						<th>born</th>
+						<th>books</th>
+					</tr>
+					{authors.map(a => (
+						<tr key={a.name}>
+							<td>{a.name}</td>
+							<td>{a.born}</td>
+							<td>{a.bookCount}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	)
 }
 
 export default Authors
--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,32 +1,48 @@
-const Books = (props) => {
-  if (!props.show) {
-    return null
-  }
+import { gql, useQuery } from '@apollo/client'
 
-  const books = []
+const ALL_BOOKS = gql`
+	query {
+		allBooks {
+			title
+			author
+			published
+		}
+	}
+`
 
-  return (
-    <div>
-      <h2>books</h2>
+const Books = props => {
+	const { loading, error, data } = useQuery(ALL_BOOKS)
+	const books = !data ? [] : data.allBooks
 
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>author</th>
-            <th>published</th>
-          </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+	if (loading) return 'Loading...'
+	if (error) return `Error! ${error.message}`
+
+	if (!props.show) {
+		return null
+	}
+
+	return (
+		<div>
+			<h2>books</h2>
+
+			<table>
+				<tbody>
+					<tr>
+						<th></th>
+						<th>author</th>
+						<th>published</th>
+					</tr>
+					{books.map(b => (
+						<tr key={b.title}>
+							<td>{b.title}</td>
+							<td>{b.author}</td>
+							<td>{b.published}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	)
 }
 
 export default Books
