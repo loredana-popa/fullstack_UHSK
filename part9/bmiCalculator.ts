75441393d4d4fb74404693906db5720f52c0e4bb
user userFamilyName <user.userfamilyname@gmail.com>
2023-08-03 12:49:26 +0300

    Task: 9.3 Command line

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,18 +1,44 @@
-const calculateBmi = (h:number, w:number): string => {
-  const bmi = w / Math.pow(h / 100, 2)
-
-  if( bmi < 18.5){
-    return 'Underweight (Unhealthy)'
-  } else if (18.5 <= bmi && bmi <= 24.9){
-    return 'Normal (healthy weight)'
-  } else if (25 <= bmi && bmi <=29.9){
-   return 'Overweight'
-  } else {
-    return 'Obese)'
-  }
-
+interface CalculateBmiValues {
+	height: number
+	weight: number
 }
 
-console.log(calculateBmi(180, 87))
+const parseArguments = (args: string[]): CalculateBmiValues => {
+	if (args.length < 4) throw new Error('Not enough arguments')
+	if (args.length > 4) throw new Error('Too many arguments')
 
-// calculateBmi(160,54)
+	if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+		return {
+			height: Number(args[2]),
+			weight: Number(args[3]),
+		}
+	} else {
+		throw new Error('Provided values are not numbers!')
+	}
+}
+
+const calculateBmi = (h: number, w: number): string => {
+	const bmi = w / Math.pow(h / 100, 2)
+
+	if (bmi < 18.5) {
+		return 'Underweight (Unhealthy)'
+	} else if (18.5 <= bmi && bmi <= 24.9) {
+		return 'Normal (healthy weight)'
+	} else if (25 <= bmi && bmi <= 29.9) {
+		return 'Overweight'
+	} else {
+		return 'Obese'
+	}
+}
+
+try {
+	const { height, weight } = parseArguments(process.argv)
+	calculateBmi(height, weight)
+	console.log(calculateBmi(height, weight))
+} catch (err: unknown) {
+	let errMessage = 'Something bad happened'
+	if (err instanceof Error) {
+		errMessage += 'Error: ' + err.message
+	}
+	console.log(errMessage)
+}
--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -1,47 +1,87 @@
 interface Result {
-  periodLength: number,
-  trainingDays: number,
-  dailyTarget: number,
-  avgTime: number,
-  success: boolean,
-  rating: number,
-  ratingDescription: string
+	periodLength: number
+	trainingDays: number
+	dailyTarget: number
+	avgTime: number
+	success: boolean
+	rating: number
+	ratingDescription: string
 }
 
-const calculateExercises = (a:number[], b:number): Result =>{
-  const periodLength = a.length
-  const trainingDays = a.filter(e => e > 0).length
-  const sumTime = a.reduce((a,b) => a + b, 0)
-  const avgTime = (sumTime/periodLength)
-  const success = avgTime >= b
-
-// rating is calculated based on the formula:
-// Total Decimal Score) / (Total Maximum Decimal Score) x (Maximum Numeric Rating)
-// here the Maximum Numeric Rating is 3
-// apply decimal rounding for rating result
-  const decimalScore = a.map((e)=> {
-    if (0<= e && e <= (b/2))
-      { return 1/3}
-    else if( (b/2) <e && e < b)
-      {return 2/3}
-    else 
-      {return 3/3}
-    }
-  )
-  const totalDecimalScore = decimalScore.reduce((a,b)=> a + b,0)
-  const totalMaxDecimalScore = (1 * periodLength)
-  const rating = Math.round(totalDecimalScore / totalMaxDecimalScore * 3)
-  const ratingDescription = (rating == 3) ? 'Congratulations! You achieved your goal!' : (rating == 2) ? 'Not that bad but could be better' : 'You need to put some effort here!'
-
-  return {
-    periodLength: periodLength,
-    trainingDays: trainingDays,
-    dailyTarget: b,
-    avgTime: avgTime,
-    success: success,
-    rating: rating,
-    ratingDescription: ratingDescription
-  }
+interface CalculateExValues {
+	rating: number
+	data: number[]
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1],2))
+const parseArgs = (args: string[]): CalculateExValues => {
+	const argsArr: string[] = process.argv.slice(3)
+	const a: number[] = argsArr.map((e) => parseInt(e))
+	const b: number = Number(process.argv[2])
+	const onlyNumbers: boolean = a.every((e) => !isNaN(e))
+
+	if (args.length < 4) throw new Error('Not enough arguments')
+
+	if (onlyNumbers && !isNaN(b)) {
+		return {
+			rating: b,
+			data: a,
+		}
+	} else {
+		throw new Error('Provided values are not numbers!')
+	}
+}
+
+const calculateExercises = (a: number, b: number[]): Result => {
+	const periodLength: number = b.length
+	const trainingDays: number = b.filter((e) => e > 0).length
+	const sumTime: number = b.reduce((a, b) => a + b, 0)
+	const avgTime: number = sumTime / periodLength
+	const success: boolean = avgTime >= a
+
+	// rating is calculated based on the formula:
+	// Total Decimal Score) / (Total Maximum Decimal Score) x (Maximum Numeric Rating)
+	// here the Maximum Numeric Rating is 3
+	// apply decimal rounding for rating result
+	const decimalScore: number[] = b.map((e) => {
+		if (0 <= e && e <= a / 2) {
+			return 1 / 3
+		} else if (a / 2 < e && e < a) {
+			return 2 / 3
+		} else {
+			return 3 / 3
+		}
+	})
+	const totalDecimalScore: number = decimalScore.reduce((a, b) => a + b, 0)
+	const totalMaxDecimalScore: number = 1 * periodLength
+	const rating: number = Math.round(
+		(totalDecimalScore / totalMaxDecimalScore) * 3
+	)
+	const ratingDescription: string =
+		rating == 3
+			? 'Congratulations! You achieved your goal!'
+			: rating == 2
+			? 'Not that bad but could be better'
+			: 'You need to put some effort here!'
+
+	return {
+		periodLength: periodLength,
+		trainingDays: trainingDays,
+		dailyTarget: a,
+		avgTime: avgTime,
+		success: success,
+		rating: rating,
+		ratingDescription: ratingDescription,
+	}
+}
+
+try {
+	const { rating, data } = parseArgs(process.argv)
+	calculateExercises(rating, data)
+	console.log(calculateExercises(rating, data))
+} catch (error: unknown) {
+	let errorMessage = 'Something had happened.'
+	if (error instanceof Error) {
+		errorMessage += 'Error: ' + error.message
+	}
+	console.log(errorMessage)
+}
