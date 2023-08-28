5a774676a0adcfcb2f4d6d93d9d64b5e6d58ef1b
user userFamilyName <user.userfamilyname@gmail.com>
2023-08-28 17:45:09 +0300

    Exercise 9.17: implemented functionality to add new diaries from the frontend; the diaries array doesn't update immediately after clicking the add button and the input areas for weather and visibility is preset

--- part9/flight-diary/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary/flight-diary-frontend/src/App.tsx
@@ -1,13 +1,24 @@
 import { useEffect, useState } from 'react';
-import { NonSensitiveDiaryEntry } from './types';
+import { NewDiaryEntry, NonSensitiveDiaryEntry, Visibility, Weather } from './types';
 import Diary from './components/Diary';
 
-import {getAllDiaries} from './services/diaryService'
+import {getAllDiaries, addNewDiary} from './services/diaryService'
 
 
 const App=()=> {
   const [diaries, setDiaries]= useState<NonSensitiveDiaryEntry[]>([
   ])
+
+  const [newDiary, setNewDiary] = useState<NewDiaryEntry>({
+    date:'',
+    weather: Weather.Sunny,
+    visibility: Visibility.Good,
+    comment:''
+  })
+  // const [date, setDate] = useState('')
+  // const[ weather, setWeather] = useState<Weather>(Weather.Sunny)
+  // const[visibility, setVisibility]= useState<Visibility>(Visibility.Good)
+  // const[comment, setComment]=useState('smth')
 
   useEffect(()=>{
     getAllDiaries().then(data=>{
@@ -16,13 +27,81 @@ const App=()=> {
     })
   },[])
 
+  const handleChange = (e:React.FormEvent<HTMLInputElement>):void => {
+		const { name, value } = e.currentTarget
+    console.log(e.currentTarget.nodeName)
+		setNewDiary(prev => {
+			return { ...prev, [name]: value }
+		})
+	}
+  const createDiary=(event:React.SyntheticEvent)=>{
+    event.preventDefault()
+      const diaryToAdd ={
+        date: newDiary.date,
+        weather: newDiary.weather,
+        visibility: newDiary.visibility,
+        comment: newDiary.comment
+      }
+
+      addNewDiary(diaryToAdd)
+
+  }
 
   console.log('Diaries are',diaries)
   
 
   return (
     <div className="App">
+      <h2>Add new entry</h2>
+      <form onSubmit={createDiary}>
+        <div>
+          <label>date 
+            <input 
+              type='text'
+              name='date' 
+              value={newDiary.date} 
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        
+        <div>
+          <label>weather
+            <input 
+              name='weather' 
+              value={newDiary.weather} 
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+       
+       <div>
+         <label>visibility 
+            <input 
+              name='visibility' 
+              value={newDiary.visibility} 
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>comment
+            <input 
+              type='text'
+              name='comment' 
+              value={newDiary.comment} 
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+
+        <button type='submit'>add</button>
+
+      </form>
+
       <h2>Diary entries</h2>
+
       {diaries.map(d => 
       <Diary diary={d} key={d.id}/>
       )}
--- part9/flight-diary/flight-diary-frontend/src/services/diaryService.ts
+++ part9/flight-diary/flight-diary-frontend/src/services/diaryService.ts
@@ -1,9 +1,15 @@
 import axios from 'axios';
-import { NonSensitiveDiaryEntry } from '../types';
+import { NewDiaryEntry, NonSensitiveDiaryEntry } from '../types';
 
 const baseUrl = 'http://localhost:3009/api/diaries';
 
 export const getAllDiaries = async () => {
 	const { data } = await axios.get<NonSensitiveDiaryEntry[]>(`${baseUrl}`);
 	return data;
+};
+
+export const addNewDiary = async (newObject: NewDiaryEntry) => {
+	const response = await axios.post(baseUrl, newObject);
+	console.log('a new diary was added to frontend: ', response.data);
+	return response.data;
 };
