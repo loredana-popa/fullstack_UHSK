c2783fbecb86d9949efeea2b6ffc135c5dd75c44
user userFamilyName <user.userfamilyname@gmail.com>
2023-08-30 20:50:38 +0300

    Task 9.21: Patientor, step2: created a page to display all info of a patient; NOTE: the ssn does not display value

---
+++ part9/patientor/patientor-frontend/src/components/PatientInfo.tsx
@@ -0,0 +1,27 @@
+ import { useParams } from "react-router-dom"
+import { Patient } from "../types"
+ 
+ interface PatientInfoProps{
+  patients : Patient[]
+ }
+
+const PatientInfo=(props: PatientInfoProps)=>{
+  const id= useParams().id
+  const patient = props.patients.find(p=> p.id === id)
+
+  return(
+    patient ?
+    
+      <div>
+        <h2>{patient.name}</h2>
+        <div>gender: {patient.gender}</div>
+        <div>date of birth: {patient.dateOfBirth}</div>
+        <div>ssn:{patient.ssn}</div>
+        <div>occupation: {patient.occupation}</div>      
+      </div>
+    :
+      <div> There are no info about this patient </div>
+  )
+}
+
+export default PatientInfo
